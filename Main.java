7ce60552621ecedb97d6f73e853d4a83b01b770b
Hannah Donovan <34870726+hannahcdonovan@users.noreply.github.com>
2019-12-02 20:41:35 -0500

    helper method for reading in problem

--- Main.java
+++ Main.java
@@ -1,5 +1,56 @@
 public class Main {
+    
+    /**
+	 * Helper method to read in the file and problem information.
+	 * 
+	 * @param filename The name of the file we want to read in.
+	 * @return A list of City objects (i.e. our problem information)
+	 */
+	public static List<City> getCityList(String filename) {
+		String data = null;
+		int dimension = 0;
+		String edgeWeightType = "";
+		List<City> cities = new ArrayList<City>();
 
+		try {
+			File initialFile = new File(filename);
+			initialFile.createNewFile();
+
+			Reader reader = new FileReader(initialFile);
+
+			BufferedReader buffReader = new BufferedReader(reader);
+
+			boolean first = true;
+			int counter = 1;
+
+			while ((data = buffReader.readLine()) != null) {
+				City city;
+				if (first) {
+					if (data.contains("EDGE_WEIGHT_TYPE")) {
+						edgeWeightType = data.split(" ")[1];
+					} else if (data.contains("DIMENSION")) {
+						dimension = Integer.parseInt(data.replace(" ", "").split(":")[1]);
+					} else if (data.contains("NODE_COORD_SECTION")) {
+						first = false;
+					}
+				} else {
+					if (counter <= dimension) {
+						String[] temp = data.trim().replace("  ", " ").replace("  ", " ").split(" ");
+						double xCoord = Double.parseDouble(temp[1].trim());
+						double yCoord = Double.parseDouble(temp[2].trim());
+						city = new City(counter, xCoord, yCoord);
+						cities.add(city);
+						counter++;
+					}
+				}
+			}
+			buffReader.close();
+		} catch (IOException e) {
+			System.out.println("IO Exception");
+		}
+		return cities;
+    }
+    
     public static void main(String[] args) {
         System.out.println("test");
     }
